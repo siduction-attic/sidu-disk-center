fec2f1d474f7281b6beabde42537e385b714a725
hama <hama@siduction.net>
2014-04-15 21:40:57 +0200

    pywwetha needs other urls

--- sidu-disk-center/urls.py
+++ sidu-disk-center/urls.py
@@ -4,14 +4,14 @@ from diskcenter import views
 
 def getPatterns():
     rc = patterns('',
-        url(r'^$', views.home, name='root'),
-        url(r'^home', views.home, name='home'),
-        url(r'^lvm', views.lvm, name='lvm'),
-        url(r'^overview', views.overview, name='overview'),
-        url(r'^physicalview', views.physicalView, name='physicalview'),
-        url(r'^logicalview', views.logicalView, name='logicalview'),
-        url(r'^snapshots', views.snapshots, name='snapshots'),
-        url(r'^wait', views.wait, name='wait')
+        url(r'/?$', views.home, name='root'),
+        url(r'/?home', views.home, name='home'),
+        url(r'/?lvm', views.lvm, name='lvm'),
+        url(r'/?overview', views.overview, name='overview'),
+        url(r'/?physicalview', views.physicalView, name='physicalview'),
+        url(r'/?logicalview', views.logicalView, name='logicalview'),
+        url(r'/?snapshots', views.snapshots, name='snapshots'),
+        url(r'/?wait', views.wait, name='wait')
         )
     return rc
                   
